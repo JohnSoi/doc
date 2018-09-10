ab51d18adad1219d98caf3861e887ed4b4594d26
JohnSoi <un.perso@yandex.ru>
2018-09-10 21:05:56 +0500

    Релиз 2

--- add.php
+++ add.php
@@ -63,6 +63,42 @@
     			}
     		}
     	}
+
+      elseif ($typePage == 2)
+      {
+        if(isset($_GET['submit']))
+        {
+          if(!empty($_GET['name']) & !empty($_GET['cost']) & !empty($_GET['bonus']))
+          {
+            $name = $_GET['name'];
+            $cost = $_GET['cost'];
+            $bonus = $_GET['bonus'];
+            $dist = $_GET['dist'];
+
+            $sql = "SELECT * FROM items WHERE name = ".$name."AND cost = ".$cost;
+            $query = mysqli_query($connection, $sql);
+
+            if(mysqli_num_rows($query) == 0){
+              $sql = "INSERT INTO items(name,cost,dist,bonus) VALUES('".$name."','".$cost."','".$dist."','".$bonus."')";
+              $query = mysqli_query($connection, $sql);
+              if($query)
+                {
+                  $message = "Добавление успешно";
+                  echo "<script>setTimeout(function(){self.location=\"input.php&flaginput=4\";}, 1500);</script>";
+                }
+              else
+                $message = "Данные не обработаны";
+            }
+            else
+            {
+              $message = "Такая услуга уже существует!";
+            }
+          }
+          else{
+              $message = "Все поля обязательны для заполнения!";
+            }         
+        }
+      }
 ?>
 
 <!DOCTYPE html>
@@ -131,7 +167,15 @@
       			echo "<h2>Добавление в стационар</h2>";
       			break;
       		case 4:
-      			echo "<h2>Добавление услуги</h2>";
+      			echo '
+                <form action="temp.php" method="GET">
+                  <p><label for="name">Название услуги<br><input name="name" type="text"></label></p>
+                  <p><label for="cost">Стоимость<br><input name="cost" type="text"></label></p>
+                  <p><label for="bonus">Бонус за предоставление<br><input name="bonus" type="text"></label></p>
+                  <p><label for="bonus">Описание<br><input name="dist" type="text"></label></p>
+                  <input type="submit" name="submit" value="Добавить">
+                </form>
+            ';
       			break;
       		default:
       			echo "<h2>Перенаправление на странцу авторизации</h2>";
